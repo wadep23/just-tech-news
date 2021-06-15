ce2e813a977b5b46e845a41c51ae4a5ab60591ec
Wade Facer <wadepfacer@gmail.com>
2021-06-15 17:37:07 -0600

    finished post model and routes

--- models/index.js
+++ models/index.js
@@ -1,4 +1,13 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// Create association between user and post
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
 module.exports = { User, Post };
